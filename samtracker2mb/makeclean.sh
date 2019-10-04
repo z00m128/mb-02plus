552732dc9c3be0d61f1a6e35ff542cefc668e83e
z00m128 <zoom@centrum.sk>
2019-10-04 19:14:02 +0200

    Linux modifications and scripts added.

---
+++ samtracker2mb/makeclean.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+rm -vf ./bin/*.pck ./bin/*.cod ./bin/*.bin
+rm -vf ./*.lst
+rm -vf ./*.tap
---
+++ samtracker2mb/makesmt2mb.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Sample Tracker 2 MB builder
+# Requirements:
+# sjasmplus
+# bin2tap
+# dirtap
+# zmakebas
+
+ASM="sjasmplus"
+FILE="smt2mb"
+
+mkdir -p bin
+
+${ASM} --lst=${FILE}.lst ${FILE}.a80
+zx7 ./bin/smt2mb.bin ./bin/smt2mb.pck
+zx7 ./bin/smt2pg4.bin ./bin/smt2pg4.pck
+zx7 ./inc/smt2title.scr ./bin/smt2scr.pck
+${ASM} --lst=runner.lst runner.a80
+${ASM} --lst=titlepic.lst titlepic.a80
+zmakebas -a 20 -n SMT2MB -o smt2mb.tap ./inc/smt2mb.bas
+bin2tap -append -o smt2mb.tap -a 24576 ./bin/smt2scr.cod
+bin2tap -append -o smt2mb.tap -a 32768 ./bin/smt2main.cod
+dirtap smt2mb.tap
