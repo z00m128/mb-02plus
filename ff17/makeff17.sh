b57358ce93cd06872dadb6027c8e5418fb25ea41
z00m128 <zoom@centrum.sk>
2019-11-22 14:19:40 +0100

    FF17: v33 - enhanced number of sec/trk.

--- ff17/makeff17.sh
+++ ff17/makeff17.sh
@@ -12,7 +12,7 @@ FILE="ff17"
 
 ${ASM} --syntax=f --lst=${FILE}.lst ${FILE}.a80
 zmakebas -a 40 -n FF17 -o 001.tap ${FILE}.bas
-bin2tap -o 002.tap -a 57344 ${FILE}.bin
+bin2tap -o 002.tap -a 63488 ${FILE}.bin
 cat 001.tap 002.tap > ${FILE}.tap
 rm 001.tap 002.tap
 dirtap ${FILE}.tap
